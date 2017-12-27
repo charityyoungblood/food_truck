228c778ede2c931baab4dafa92f24b8c53408c82
charityyoungblood <youngblood.charity@gmail.com>
2017-12-26 18:23:29 -0800

    added condition and add_items(items) method to initialize method in food-truck.rb

--- lib/food-truck.rb
+++ lib/food-truck.rb
@@ -1,3 +1,10 @@
 class FoodTruck
-  
+  def initialize(name, hours, items = nil) # if you allow an argument to have a default value, be sure to include if statement with "if (argument) is != nil
+    @name = name
+    @hours = hours 
+    items = [] # shrimp platter $12 
+    self.add_items(items) if items != nil # we will need to create this method below 
+  end 
+
+
 end 
